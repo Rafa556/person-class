c8803c8d4da085476a7760979e52140bc4aae044
fyoussef <fillipyuou@gmail.com>
2025-03-25 11:26:04 -0300

    add object to example

--- main.ts
+++ main.ts
@@ -1,47 +1,63 @@
-class Person { /*Classe*/
+class Person {
+  /*Classe*/
 
-    private name: string;/*Private: só pode ser usado dentro da classe */
-    private currentAge: number;
+  private name: string; /*Private: só pode ser usado dentro da classe */
+  private currentAge: number;
 
-    create(name: string, age: number): void { /*Parametros dentro dos () / Tipagem do método "Void", pois não retorna um valor */
-        this.name = name;
-        this.currentAge = age;
+  create(name: string, age: number): string {
+    /*Parametros dentro dos () / Tipagem do método "Void", pois não retorna um valor */
+    this.name = name;
+    this.currentAge = age;
 
-        const messageUpper = this.upperMessage(name);
-        const birthYear = this.calculateBirthDate(age);
+    const messageUpper = this.toUpperCase(name);
+    const birthYear = this.calculateBirthDate(age);
 
-        const result = `Name: ${messageUpper}, Birth Year: ${birthYear}, Age: ${age}`;
-        console.log(result);
-    }
+    const result = `Name: ${messageUpper}, Birth Year: ${birthYear}, Age: ${age}`;
+    return result;
+  }
 
-    updateAge(newAge: number): void {
-        const currentYear = new Date().getFullYear();
-        const targetYear = currentYear + (newAge - this.currentAge);
+  getYearOfAge(age: number): string {
+    const currentYear = new Date().getFullYear();
+    const targetYear = currentYear + (age - this.currentAge);
 
-        this.currentAge = newAge;
+    const messageUpper = this.toUpperCase(this.name);
+    const result = `Name: ${messageUpper}, New Birth Year: ${age}, in the year ${targetYear}`;
+    return result;
+  }
 
-        const messageUpper = this.upperMessage(this.name);
-        const result = `Name: ${messageUpper}, New Birth Year: ${newAge}, in the year ${targetYear}`;
-        console.log(result);
-    }
+  calculateBirthDate(age: number): number {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    return year - age;
+  }
 
-    calculateBirthDate(age: number): number {
-        let currentDate = new Date();
-        let year = currentDate.getFullYear();
-        return year - age;
-
-    }
-
-    upperMessage(message: string): string {
-        return message.toUpperCase();
-    }
+  toUpperCase(input: string): string {
+    return input.toUpperCase();
+  }
 }
 
 function main() {
-    const person = new Person();
-    person.create("Rafael", 23);
+  const person = new Person();
+  const result = person.create("Rafael", 23);
+  const yearOfAge = person.getYearOfAge(28);
 
-    person.updateAge(24);
+  const person2 = {
+    name: "Rafael",
+    age: 23,
+    create(input: string) {
+      const name = input.toUpperCase();
+      return name;
+    },
+    info: {
+      height: 1.75,
+      weight: 80,
+      details: {},
+    },
+  };
+
+  console.log(person);
+  console.log(person2.create("Rafael"));
+  console.log(person2.info.height);
 }
 
 main();
