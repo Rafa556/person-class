82b0344dffa8b0b7e9c4b2526d018b20598cdf87
Rafa556 <rafademaior@outlook.com>
2025-04-08 08:31:31 -0300

    sorteador

---
+++ Fundamentos/organizacao.js
@@ -0,0 +1,30 @@
+// let idade = 31;
+// console.log(typeof 31);
+// console.log(typeof idade);
+
+// let salario = 5478.43;
+// console.log(typeof salario);
+
+// let estaChovendo = true;
+// console.log(typeof estaChovendo);
+
+// var a = 3;
+// let b = 4;
+
+// var a = 20;
+// b = 40;
+
+// const c = 400;
+
+// const peso1 = 1.0;
+// const peso2 = Number("2.0");
+
+// console.log(peso1, peso2);
+// console.log(Number.isInteger(peso1));
+// console.log(Number.isInteger(peso2));
+
+// const avaliacao1 = 9.5;
+// const avaliacao2 = 6.5;
+
+// const total = avaliacao1 * peso1 + avaliacao2 * peso2
+// const media = total / (peso1 + peso2)
